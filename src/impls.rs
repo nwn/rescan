c06ab36b702341d0609ed4cd1a9aba75a2343215
Nathan Wiebe Neufeldt <wn.nathan@gmail.com>
2021-11-24 23:33:15 -0500

    Impl DefaultScan for floats and IP address types

--- src/impls.rs
+++ src/impls.rs
@@ -89,3 +89,154 @@ impl_default_scan!(std::num::NonZeroI32, INT_REGEX);
 impl_default_scan!(std::num::NonZeroI64, INT_REGEX);
 impl_default_scan!(std::num::NonZeroI128, INT_REGEX);
 impl_default_scan!(std::num::NonZeroIsize, INT_REGEX);
+
+// Regex matching the grammar used by `f32`'s and `f64`'s `FromStr` implementation:
+// https://doc.rust-lang.org/stable/std/primitive.f32.html#method.from_str
+const FLOAT_REGEX: &str = r"[+-]?(?i:inf|nan|(?:[0-9]+\.?[0-9]*|\.[0-9]+)(?:e[+-]?[0-9]+)?)";
+impl_default_scan!(f32, FLOAT_REGEX);
+impl_default_scan!(f64, FLOAT_REGEX);
+
+// Regex approximately matching IPv4 addresses in their "dotted decimal" notation.
+// This pattern is exact except that any 3-digit decimal value is accepted in each
+// octet position, rather than the proper range of 0..=255.
+const IPV4_REGEX: &str = r"(?:[0-9]{1,3}\.){3}[0-9]{1,3}";
+impl_default_scan!(std::net::Ipv4Addr, IPV4_REGEX);
+
+// Regex approximately matching IPv6 addresses according to IETF RFC 4291 Section 2.2.
+// This pattern is exact except that:
+//  - any 3-digit decimal value is accepted in the octet positions of an embedded IPv4 address
+//  - a compressed address (i.e. one containing "::") may consist of more hextet fields than
+//    is correct, though still bounded.
+// https://datatracker.ietf.org/doc/html/rfc4291#section-2.2
+// const IPV6_REGEX: &str = r"(?:[[:xdigit:]]{0,4}:){2,7}(?:[[:xdigit:]]{0,4}|(?:[0-9]{1,3}\.){3}[0-9]{1,3})";
+const IPV6_REGEX: &str = r"(?x)
+    (?: [[:xdigit:]]{1,4} (?::[[:xdigit:]]{1,4}){7} ) | # Uncompressed
+    (?: [[:xdigit:]]{1,4} (?::[[:xdigit:]]{1,4}){5} :[0-9]{1,3} (?:\.[0-9]{1,3}){3} ) | # Uncompressed with embedded IPv4
+    (?:
+        (?: (?:[[:xdigit:]]{1,4}:)+ | : )
+        (?:
+            (?: : (?:(?:[0-9]{1,3}\.){3}[0-9]{1,3})? ) | # Compressed at end (with optional embedded IPv4)
+            (?: (?::[[:xdigit:]]{1,4})+ (?::(?:[0-9]{1,3}\.){3}[0-9]{1,3})? ) # Compressed at start or middle (with optional embedded IPv4)
+        )
+    )";
+impl_default_scan!(std::net::Ipv6Addr, IPV6_REGEX);
+
+// Regex approximately matching both IPv4 and IPv6 addresses. This pattern is the union of the
+// [`IPV4_REGEX`] and [`IPV6_REGEX`] patterns, and the same caveats apply.
+const IP_REGEX: &str = r"(?x)
+    (?: (?:[0-9]{1,3}\.){3}[0-9]{1,3} ) | # IPv4
+    (?: [[:xdigit:]]{1,4} (?::[[:xdigit:]]{1,4}){7} ) | # Uncompressed IPv6
+    (?: [[:xdigit:]]{1,4} (?::[[:xdigit:]]{1,4}){5} :[0-9]{1,3} (?:\.[0-9]{1,3}){3} ) | # Uncompressed IPv6 with embedded IPv4
+    (?:
+        (?: (?:[[:xdigit:]]{1,4}:)+ | : )
+        (?:
+            (?: : (?:(?:[0-9]{1,3}\.){3}[0-9]{1,3})? ) | # IPv6 compressed at end (with optional embedded IPv4)
+            (?: (?::[[:xdigit:]]{1,4})+ (?::(?:[0-9]{1,3}\.){3}[0-9]{1,3})? ) # IPv6 compressed at start or middle (with optional embedded IPv4)
+        )
+    )";
+impl_default_scan!(std::net::IpAddr, IP_REGEX);
+
+#[cfg(test)]
+mod test {
+    use crate::DefaultScan;
+
+    fn default_regex<T: DefaultScan>() -> regex::Regex {
+        regex::Regex::new(&format!(r"\A(?:{})\z", T::DEFAULT_REGEX)).unwrap()
+    }
+
+    #[test]
+    fn ipv4() {
+        let re = default_regex::<std::net::Ipv4Addr>();
+        assert!(!re.is_match(""));
+        assert!(!re.is_match("0.0.0"));
+        assert!(!re.is_match(".0.0.0"));
+        assert!(!re.is_match("0..0.0"));
+        assert!(!re.is_match("0.0..0"));
+        assert!(!re.is_match("0.0.0."));
+        assert!(!re.is_match("0.0.0.0."));
+        assert!(!re.is_match("0.0.0.0.0"));
+        assert!(!re.is_match("1000.0.0.0"));
+        assert!(!re.is_match("0.0.0.1000"));
+
+        assert!(re.is_match("0.0.0.0"));
+        assert!(re.is_match("255.0.0.0"));
+        assert!(re.is_match("0.255.0.0"));
+        assert!(re.is_match("0.0.255.0"));
+        assert!(re.is_match("0.0.0.255"));
+        assert!(re.is_match("255.255.255.255"));
+        assert!(re.is_match("10.27.0.255"));
+        assert!(re.is_match("192.168.0.1"));
+        assert!(re.is_match("010.0.00.001"));
+    }
+
+    #[test]
+    fn ipv6() {
+        let re = default_regex::<std::net::Ipv6Addr>();
+        assert!(!re.is_match(""));
+        assert!(!re.is_match(":"));
+        assert!(!re.is_match(":1"));
+        assert!(!re.is_match("192.168.0.1"));
+        assert!(!re.is_match(":192.168.0.1"));
+        assert!(!re.is_match("::10.0.0.0.1"));
+        assert!(!re.is_match("::1000.0.0.0"));
+        assert!(!re.is_match(":ff01::101"));
+        assert!(!re.is_match("ff01::101:"));
+
+        assert!(re.is_match("::"));
+        assert!(re.is_match("::1"));
+        assert!(re.is_match("::192.168.0.1"));
+        assert!(re.is_match("::1:192.168.0.1"));
+        assert!(re.is_match("abcd:ef01:2345:6789:abcd:ef01:2345:6789"));
+        assert!(re.is_match("ABCD:EF01:2345:6789:ABCD:EF01:2345:6789"));
+        assert!(re.is_match("abcd:ef01:2345:6789:abcd:ef01:192.168.0.1"));
+        assert!(re.is_match("2001:db8:0:0:8:800:200c:417a"));
+        assert!(re.is_match("2001:db8::8:800:200c:417a"));
+        assert!(re.is_match("ff01::101"));
+        assert!(re.is_match("ff01::101:192.168.0.1"));
+    }
+
+    #[test]
+    fn ip() {
+        let re = default_regex::<std::net::IpAddr>();
+        assert!(!re.is_match(""));
+        assert!(!re.is_match("0.0.0"));
+        assert!(!re.is_match(".0.0.0"));
+        assert!(!re.is_match("0..0.0"));
+        assert!(!re.is_match("0.0..0"));
+        assert!(!re.is_match("0.0.0."));
+        assert!(!re.is_match("0.0.0.0."));
+        assert!(!re.is_match("0.0.0.0.0"));
+        assert!(!re.is_match("1000.0.0.0"));
+        assert!(!re.is_match("0.0.0.1000"));
+
+        assert!(re.is_match("0.0.0.0"));
+        assert!(re.is_match("255.0.0.0"));
+        assert!(re.is_match("0.255.0.0"));
+        assert!(re.is_match("0.0.255.0"));
+        assert!(re.is_match("0.0.0.255"));
+        assert!(re.is_match("255.255.255.255"));
+        assert!(re.is_match("10.27.0.255"));
+        assert!(re.is_match("192.168.0.1"));
+        assert!(re.is_match("010.0.00.001"));
+
+        assert!(!re.is_match(":"));
+        assert!(!re.is_match(":1"));
+        assert!(!re.is_match(":192.168.0.1"));
+        assert!(!re.is_match("::10.0.0.0.1"));
+        assert!(!re.is_match("::1000.0.0.0"));
+        assert!(!re.is_match(":ff01::101"));
+        assert!(!re.is_match("ff01::101:"));
+
+        assert!(re.is_match("::"));
+        assert!(re.is_match("::1"));
+        assert!(re.is_match("::192.168.0.1"));
+        assert!(re.is_match("::1:192.168.0.1"));
+        assert!(re.is_match("abcd:ef01:2345:6789:abcd:ef01:2345:6789"));
+        assert!(re.is_match("ABCD:EF01:2345:6789:ABCD:EF01:2345:6789"));
+        assert!(re.is_match("abcd:ef01:2345:6789:abcd:ef01:192.168.0.1"));
+        assert!(re.is_match("2001:db8:0:0:8:800:200c:417a"));
+        assert!(re.is_match("2001:db8::8:800:200c:417a"));
+        assert!(re.is_match("ff01::101"));
+        assert!(re.is_match("ff01::101:192.168.0.1"));
+    }
+}
